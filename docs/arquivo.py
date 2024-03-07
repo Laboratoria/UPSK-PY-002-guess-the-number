834a8017bc419dfa27ef93b77fa7526178053bd2
MailaFAP <maila_3@hotmail.com>
2024-03-07 12:18:54 -0300

    descontrui a função de advinhar_número e montei outras para fazer palpite, armazenar palpite, analisar o palpite e o loop para analisar quem está jogando e ter um revesamento entre jogador e computador

--- docs/arquivo.py
+++ docs/arquivo.py
@@ -1,41 +1,67 @@
 import random
 
+   
+# função para sortear um número aleatório 
 def adivinhar_numero():
     numero_secreto = random.randint(1, 100)
-    tentativas_p = []
-    tentativas_c = []
+    return numero_secreto
+
+# boas vindas ao jogo
+print("Bem-vindo ao jogo de adivinhar o número!")
+# perguntar o nome do jogador humano
+nome_jogador = input("Qual é o seu nome? ")
+
+# indicar início do jogo    
+print("Vamos começar!")
+
+# definição de jogadores
+jogador = [ 
+            {
+                "nome": "Computador",
+                "palpites_computador": []
+                },
+
+            {
+                "nome": nome_jogador,
+                "palpites_humano": []
+                }
+            ]
+
+# função para fazer um palpite
+def fazer_palpite(jogador,palpite):
+    if jogador["nome"] == "Computador":
+        palpite = random.randint(1, 100)
+        print(f'{jogador["nome"]} palpitou {palpite}')
+    else:
+        palpite = int(input(f'{jogador["nome"]}, faça seu palpite: '))
+
+#armazenar os palpites no array palpite de cada jogador
+def armazenar_palpite(jogador, palpite):
+    if jogador["nome"] == "Computador":
+        jogador["palpites_computador"].append(palpite)
+    else:
+        jogador["palpites_humano"].append(palpite)
+        
+#função para analisar o palpite e printar se foi alto ou baixo        
+def analisar_palpite(numero_secreto, palpite): 
+    if palpite > numero_secreto: 
+        print("O palpite foi maior que o número sorteado" ) 
+    elif palpite < numero_secreto: 
+        print("O palpite foi menor que o número sorteado")  
+    else: 
+        print("Parabéns! Você acertou o número sorteado") 
+        
+
+
+#loop para alternar entre jogadas do humano e computador    
+numero_secreto = adivinhar_numero()
+jogador = jogador[1]
+while True:
+    palpite = fazer_palpite(jogador, 0)  
+    armazenar_palpite(jogador, palpite)
+    analisar_palpite(jogador, 0)
     
-    jogador = 'p'
+    if palpite == numero_secreto:
+        break 
     
-    while True:
-        if jogador == 'p':
-            palpite_p = int(input("Jogador, digite um número entre 1 e 100: "))
-            tentativas_p.append(palpite_p)
-
-            if palpite_p < numero_secreto:
-                print("Palpite muito baixo. Tente novamente.")
-            elif palpite_p > numero_secreto:
-                print("Palpite muito alto. Tente novamente.")
-            else:
-                print("Parabéns! Você acertou o número secreto!")
-                print("Tentativas: {}" .format(tentativas_p))
-                break
-            jogador = 'c'
-            print('-=-'*20)
-            
-        elif jogador == 'c':
-            palpite_c = random.randint(1, 100)
-            tentativas_c.append(palpite_c)
-
-            if palpite_c < numero_secreto:
-                print("O computador chutou {} e foi muito baixo." .format(palpite_c))
-            elif palpite_c > numero_secreto:
-                print("O computador chutou {} e foi muito alto.".format(palpite_c))
-            else:
-                print("O computador acertou o número secreto!")
-                print("Tentativas do computador: {}" .format(tentativas_c))
-                break
-            jogador = 'p'
-            print('-=-'*20)
-
-adivinhar_numero()
+    jogador = jogador[0]
